e6514551ae8819b6c6e14930972c11f2a2c45eee
Peter Sutcliffe <peter.sutcliffe81@gmail.com>
2017-02-07 14:31:33 -0500

    Updated map marker

--- sbprocess.py
+++ sbprocess.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 07 15:35:12 2016
+Created on Wed Jan 06 15:35:12 2016
 
-@author: Rachael
+@author: Peter
 """
 import ast
+from bs4 import BeautifulSoup
 import re
 import folium
 import string
@@ -21,7 +22,7 @@ import matplotlib.pyplot as plt
 
 SB_coordinates=(41.6764,-86.25528)  # South Bend coordinates for the map to start at
 
-superb = pd.read_csv('D:\Documents\PythonProjects\superbowl.csv')
+superb = pd.read_csv('D:\Documents\PythonProjects\SuperBowl2017\superbowl.csv')
 superb['createdat'] = pd.to_datetime(pd.Series(superb['created_at']))
 superb.set_index('createdat', drop=False, inplace=True)
 superb.index = superb.index.tz_localize('GMT').tz_convert('EST')
@@ -35,12 +36,18 @@ Maps
 """""
 
 
-locations = pd.read_csv('D:\Documents\PythonProjects\superbowl.csv', usecols=['geo']).dropna()
+locations = pd.read_csv('D:\Documents\PythonProjects\SuperBowl2017\superbowl.csv', usecols=['geo']).dropna()
+loctext=pd.read_csv('D:\Documents\PythonProjects\SuperBowl2017\superbowl.csv', usecols=['geo','text']).dropna()
 geos = []
-for location in locations.values:
+ltext=[]
+##for location in locations.values:
+##  
+##  geos.append(ast.literal_eval(location[0])['coordinates'])
   
-  geos.append(ast.literal_eval(location[0])['coordinates'])
-
+for location in loctext.values:
+  
+  geos.append(ast.literal_eval(location[1])['coordinates'])
+  ltext.append(location[0])
 
 sbmap = folium.Map(location=SB_coordinates, zoom_start=4)
 lats=[float(item[0]) for item in geos]
@@ -54,34 +61,86 @@ tweet_map = folium.Map(location=SB_coordinates, tiles='Mapbox Bright', zoom_star
 
 # add markers
 for geo in geos:
-  tweet_map.circle_marker(location=geo, radius=250)
+  folium.Marker(location=geo,icon=folium.Icon(color='cloud')).add_to(tweet_map)
 
-tweet_map.create_map(path='map.html')
+
+tweet_map.save('map.html')
+
+
 """""
-Word frequency
+Tweet source
 """""
-tokens=[]
-stop = stopwords.words('english')
-text = superb['text']
-for texts in text.values:
-  tokens.extend([word.lower().strip(':,."-') for word in texts.split()])
 
-filtered_tokens = [word.translate(None,string.punctuation) for word in tokens]
-
-freq_dist = nltk.FreqDist(filtered_tokens)
-print freq_dist.plot(25)
-
-
-
-
-tweets_by_country = superb['source'].value_counts()
+tweets_by_source = superb['source'].value_counts()
 
 fig, ax = plt.subplots()
 ax.tick_params(axis='x', labelsize=15)
 ax.tick_params(axis='y', labelsize=10)
-ax.set_xlabel('Source', fontsize=15)
+ax.set_xlabel('Source', fontsize=15, rotation=45)
 ax.set_ylabel('Number of tweets' , fontsize=15)
-ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
-tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+ax.set_title('Top 5 sources', fontsize=15, fontweight='bold')
+tweets_by_source[:5].plot(ax=ax, kind='bar', color='blue')
 plt.show()
+
+
+"""""
+Word frequency
+"""""
+
+##tokens=[]
+##stop = stopwords.words('english')
+##text = superb['text']
+##for texts in text.values:
+##  tokens.extend([word.lower().strip(':,."-') for word in texts.split()])
+##
+##filtered_tokens = [word.translate(None,string.punctuation) for word in tokens]
+##
+##freq_dist = nltk.FreqDist(filtered_tokens)
+##print freq_dist.plot(25)
+
+
+"""""
+Text analysis
+"""""
+
+def review_to_words( raw_review ):
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(raw_review).get_text() 
+    #
+    # 2. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", review_text) 
+    #
+    # 3. Convert to lower case, split into individual words
+    words = letters_only.lower().split()                             
+    #
+    # 4. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))                  
+    # 
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]   
+    #
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( meaningful_words ))
+
+# Get the number of reviews based on the dataframe column size
+num_reviews = superb["text"].size
+
+### Initialize an empty list to hold the clean reviews
+##clean_superb = []
+##
+##for i in xrange( 0, num_reviews ):
+##    # Call our function for each one, and add the result to the list of
+##    # clean reviews
+##    clean_superb.append( review_to_words( superb["text"][i] ) )
+
+
+
+
+
 
